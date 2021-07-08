c0631319562d5491aff7317d791eb9b685fab343
mohsinali-Iot <mohsinali481997@gmail.com>
2021-07-08 15:56:33 +0500

    first

---
+++ todos/lib/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Todos extends StatefulWidget {
+
+  @override
+  _TodosState createState() => _TodosState();
+}
+
+class _TodosState extends State<Todos> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("TODOS"),
+      
+    );
+  }
+}
